99c77c54c33d84ce66889c87282455f4aaffecd7
Andy Tang <tangandy@umich.edu>
2019-06-26 19:56:56 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
+
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = "https://cstarleague.com/dota2/standings?division=Varsity&year=2018-2019"
 r = requests.get(url)
@@ -13,8 +15,12 @@ data2 = r2.text
 soup2 = BeautifulSoup(data2, features="html.parser")
 teamsD2 = soup2.findAll("div", {"class": "match-team"})
 
-def printPlayerInfo(players):
+teams_dict = {}
+
+def printPlayerInfo(players, team_data):
+  players_array = []
   for player in players:
+    player_data = {}
     link = player.find('a')
     player_name = link.text
     print(player_name) 
@@ -23,22 +29,37 @@ def printPlayerInfo(players):
     index = title.find("STEAM")
     steam_id = title[index:]
     print(steam_id)
+
+    player_data["player_name"] = player_name
+    player_data["steam_id"] = steam_id
+    players_array.append(player_data)
+  team_data["players"] = players_array
   return
 
-def getPlayers(team_num):
+def getPlayers(team_num, team_data):
   url = "https://cstarleague.com/dota2/teams/" + str(team_num)
   r = requests.get(url)
   data = r.text
   soup = BeautifulSoup(data, features="html.parser")
   players = soup.findAll("span", {"class": "tool-tip"})
-  printPlayerInfo(players)
+  printPlayerInfo(players, team_data)
   print("")
   return
 
+def fix_name(team_name):
+  fixed_string = team_name.replace(".", ",")
+  last_index = len(team_name) - 1
+  check_last = fixed_string[last_index]
+  if(check_last == " "):
+    fixed_string = fixed_string[:last_index]
+  return fixed_string
+
 def printTeamInfo(teams):
   for team in teams:
+    team_data = {}
     team_uni = team.find("p", {"class": "truncate-text small"})
-    print("TEAM UNIVERSITY: " + team_uni.text)
+    team_uni = team_uni.text
+    print("TEAM UNIVERSITY: " + team_uni)
     h3_container = team.find("h3")
     team_name = h3_container.text
     print("TEAM NAME: " + team_name)
@@ -46,8 +67,11 @@ def printTeamInfo(teams):
     team_num = link.get('href')
     team_num = team_num[13:]
     print("TEAM NUMBER: " + team_num)
-    # print("")
-    getPlayers(team_num)
+    team_data["team_uni"] = team_uni
+    team_data["team_num"] = team_num
+    getPlayers(team_num, team_data)
+    team_name_fixed = fix_name(team_name)
+    teams_dict[team_name_fixed] = team_data
   return
 
 print('-' * 12)
@@ -55,10 +79,11 @@ print("DIVISION ONE")
 print('-' * 12)
 print("")
 printTeamInfo(teamsD1)
+teams_json = json.dumps(teams_dict)
+print(teams_json)
 
-print('-' * 12)
-print("DIVISION TWO")
-print('-' * 12)
-print("")
-printTeamInfo(teamsD2)
-
+# print('-' * 12)
+# print("DIVISION TWO")
+# print('-' * 12)
+# print("")
+# printTeamInfo(teamsD2)
